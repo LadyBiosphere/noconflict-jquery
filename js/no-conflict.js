c3cc46e5f9f10bc79d5ff029b047445b21adcde9
unknown <nishita.wojnar@KEN20050150.wellington.natcoll.ac.nz>
2017-05-03 11:37:54 +1200

    Add interactivity.

---
+++ js/no-conflict.js
@@ -0,0 +1,20 @@
+// Only do this when there are unknown plugins or JS libraries being used, e.g. WordPress
+// 'noConflict()' allows you to use the '$' that jQuery requires
+jQuery.noConflict();
+
+// Immediately after 'noConflict', you MUST write your jQuery
+// Note: The line below is a modified '$(document).ready(function ( )) {' 
+jQuery(document).ready(function ($) {
+    $(".girl").click(function () {
+        $(".girl + label").css("background-color", "#555");
+        $(".girl + label").css("color", "#fff");
+        $(".boy + label").css("background-color", "#fff");
+        $(".boy + label").css("color", "#000");
+    });
+    $(".boy").click(function () {
+        $(".boy + label").css("background-color", "#555");
+        $(".boy + label").css("color", "#fff");
+        $(".girl + label").css("background-color", "#fff");
+        $(".girl + label").css("color", "#000");
+    });
+});
